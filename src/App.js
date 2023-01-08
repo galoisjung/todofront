6434682fe9c05d82169596ec5e3acbe89888ebe1
galoi <galoisjung@gmail.com>
2023-01-08 22:23:30 +0900

    modify 구현

--- src/App.js
+++ src/App.js
@@ -5,51 +5,59 @@ import {Container, List, ListItemText, Paper} from "@material-ui/core";
 import AddTodo from "./AddTodo";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [{id: "0", title: "Hello World 1", done: true},
-                {id: "1", title: "Hello World 2", done: false},
-            ],
-        };
-    }
+    // componentDidMount() {
+    //     const requestOptions = {
+    //         method: "GET",
+    //         headers: {"Content-Type": "application/json"}
+    //     };
+    //
+    //     fetch("http://localhost:8080/todo", requestOptions)
+    //         .then((response) = response.json())
+    //         .then(
+    //             (response) =>
+    //                 this.setState({
+    //                     items: response.data,
+    //                 });
+    // }
 
-    add = (item) => {
-        const thisItems = this.state.items;
-        item.id = "ID-" + thisItems.length;
-        item.done = false;
-        thisItems.push(item);
-        this.setState({items: thisItems});
-        console.log("items: ", this.state.items);
-    }
 
-    delete = (item) => {
-        const thisItems = this.state.items;
-        console.log("Before Update Items : ", this.state.items)
-        const newItems = thisItems.filter(e => e.id !== item.id);
-        this.setState({items : newItems}, () => {
-            console.log("Update Items : ", this.state.items)
-        });
-    }
+add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items: thisItems});
+    console.log("items: ", this.state.items);
+}
 
-    render() {
-        let todoItems = this.state.items.length > 0 && (
-            <Paper style={{margin: 16}}>
-                <List>
-                    {this.state.items.map((item, idx) => (
-                        <Todo item={item} key={item.id} delete={this.delete}/>))}
-                </List>
-            </Paper>
-        );
-        return (
-            <div className="App">
-                <Container maxWidth="md">
-                    <AddTodo add={this.add}/>
-                    <div className="TodoList">{todoItems}</div>
-                </Container>
-            </div>
-        );
-    }
+delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items)
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items: newItems}, () => {
+        console.log("Update Items : ", this.state.items)
+    });
+}
+
+render()
+{
+    let todoItems = this.state.items.length > 0 && (
+        <Paper style={{margin: 16}}>
+            <List>
+                {this.state.items.map((item, idx) => (
+                    <Todo item={item} key={item.id} delete={this.delete}/>))}
+            </List>
+        </Paper>
+    );
+    return (
+        <div className="App">
+            <Container maxWidth="md">
+                <AddTodo add={this.add}/>
+                <div className="TodoList">{todoItems}</div>
+            </Container>
+        </div>
+    );
+}
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -5,22 +5,53 @@ import {Delete, DeleteOutlined, DonutLarge} from "@material-ui/icons";
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {item: props.item};
+        this.state = {item: props.item, readOnly: true};
         this.delete = props.delete;
     }
 
-    deleteEventHandler = () =>{
+    deleteEventHandler = () => {
         this.delete(this.state.item)
+    }
+
+    offReadOnlyMode = () => {
+        console.log("Event!", this.state.readOnly)
+        this.setState({readOnly: false}, () => {
+            console.log("ReadOnly? ", this.state.readOnly)
+        });
+    }
+
+    enterKeyEventHandler = (e) => {
+        if(e.key == "Enter"){
+            this.setState({readOnly:true});
+        }
+    };
+
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({item: thisItem})
+    }
+
+    checkbokEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.done = !thisItem.done;
+        this.setState({item: thisItem})
     }
 
     render() {
         const item = this.state.item;
         return (
             <ListItem>
-                <Checkbox checked={item.done} disableRipple/>
+                <Checkbox checked={item.done} onChange={this.checkbokEventHandler}/>
                 <ListItemText>
                     <InputBase
-                        inputProps={{"aria-label": "naked"}}
+                        inputProps={{
+                            "aria-label": "naked",
+                            readOnly: this.state.readOnly
+                        }}
+                        onClick={this.offReadOnlyMode}
+                        onKeyPress={this.enterKeyEventHandler}
+                        onChange={this.editEventHandler}
                         type="text"
                         id={item.id}
                         name={item.id}
